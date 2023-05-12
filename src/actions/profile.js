6b402f79346b727f597092b65778cf3ac926b317
madhaclg <128830023+madhaclg@users.noreply.github.com>
2023-05-12 21:29:31 +0530

    Update profile.js url add

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -3,7 +3,7 @@ import { PROFILES_LOADED, PROFILES_ERROR, HISTORY_LOADED, HISTORY_ERROR, TRANSFE
 
 export const loadProfile = () => async dispatch => {
     try {
-        const res = await axios.get('/api/allUsers')
+        const res = await axios.get(`${Url}/api/allUsers`)
         console.log(res);
         dispatch({
             type: PROFILES_LOADED,
@@ -19,7 +19,7 @@ export const loadProfile = () => async dispatch => {
 
 export const loadHistory = () => async dispatch => {
     try {
-        const res = await axios.get('/api/history')
+            const res = await axios.get(`${Url}/api/history`)
         dispatch({
             type: HISTORY_LOADED,
             payload: res.data
@@ -40,7 +40,7 @@ export const transfer = ({ From, To, amount }, history) => async dispatch => {
     }
     const body = JSON.stringify({ From, To, amount })
     try {
-        const res = await axios.post('/api/transaction', body, config)
+        const res = await axios.post(`${Url}/api/transaction`, body, config)
         dispatch({
             type: TRANSFER_SUCCESS,
             payload: res.data
